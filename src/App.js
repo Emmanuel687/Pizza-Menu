293604d068113ae32bd3530fd8db8d3686ccdc81
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-06-11 12:31:33 +0300

    Dev:-Adde list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 
 function App() {
+
+  const silentDevelopers = [
+    { name: "Kate", progLang: "Python", experienceYears: 2.5 },
+    { name: "Gibson", progLang: "Go", experienceYears: 0.5 },
+    { name: "Amos", progLang: "Javascript", experienceYears: 2 },
+    { name: "Collis", progLang: "PHP", experienceYears: 1 },
+    { name: "Imran", progLang: "Java", experienceYears: 1 },
+    { name: "Doreen", progLang: "C", experienceYears: 1 },
+  ];
+
   return (
+
     <div>
-     <h1>Hello React</h1>
+      {silentDevelopers.map((silentDeveloper,index) => (
+        
+        <p key={index}>
+          Developer name: {silentDeveloper.name} Tech used
+          {silentDeveloper.progLang} Experince years:
+          {silentDeveloper.experienceYears}
+        </p>
+      ))}
     </div>
   );
 }
