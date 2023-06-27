fbc176cf504078d131249903914730b58b596834
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-06-27 13:01:06 +0300

    Dev:-Modified App file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 
 function App() {
 
-  const silentDevelopers = [
-    { name: "Kate", progLang: "Python", experienceYears: 2.5 },
-    { name: "Gibson", progLang: "Go", experienceYears: 0.5 },
-    { name: "Amos", progLang: "Javascript", experienceYears: 2 },
-    { name: "Collis", progLang: "PHP", experienceYears: 1 },
-    { name: "Imran", progLang: "Java", experienceYears: 1 },
-    { name: "Doreen", progLang: "C", experienceYears: 1 },
-  ];
+  
 
   return (
 
     <div>
-    <h1>Hello</h1>
     </div>
   );
 }
